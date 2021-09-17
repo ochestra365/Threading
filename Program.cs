ac3eca1db4a43c45a9c2fa9692674b4515644cdf
Park Seng Chul <ochestra365@naver.com>
2021-09-17 11:08:24 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().DoTest();
+        }
+
+        private void DoTest()
+        {
+            Thread t1 = new Thread(new ThreadStart(Run));
+            t1.Start();
+
+            Run();
+        }
+
+        private void Run()
+        {
+            Console.WriteLine("Thread#{0}: Begin", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(3000);
+            Console.WriteLine("Thread#{0}: End", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
